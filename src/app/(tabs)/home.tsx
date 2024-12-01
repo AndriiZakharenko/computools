fec09c87240db2ea93abed5e86c12b1cdbb74668
Andrii Zakharenko <andriizakharenko@gmail.com>
2024-12-01 14:09:27 +0200

    feat: profile

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -16,8 +16,10 @@ import EmptyState from "../../components/EmptyState";
 import useAppwrite from "../../lib/useAppwrite";
 import { getAllPosts, getLatestPosts } from "../../lib/appwrite";
 import VideoCard from "../../components/VideoCard";
+import { useGlobalContext } from "../../context/GlobalProvider";
 
 const Home = () => {
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
   const { data: posts, refetch } = useAppwrite(getAllPosts);
   const { data: latestPosts } = useAppwrite(getLatestPosts);
 
@@ -40,10 +42,10 @@ const Home = () => {
             <View className="justify-between items-start flex-row mb-6">
               <View>
                 <Text className="text-sm color-secondary-grey font-arial_regular">
-                  Welcome Back
+                  Welcome back,
                 </Text>
                 <Text className="test-2xl font-arial_regular color-secondary-white">
-                  Computools
+                  {user?.username}
                 </Text>
               </View>
               <View className="mt-1.5">
--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,10 +1,81 @@
-import { View, Text } from 'react-native';
+import { FlatList, Image, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import EmptyState from "../../components/EmptyState";
+import useAppwrite from "../../lib/useAppwrite";
+import { getUserPosts, signOut } from "../../lib/appwrite";
+import VideoCard from "../../components/VideoCard";
+import { useGlobalContext } from "../../context/GlobalProvider";
+import { icons } from "../../constans";
+import InfoBox from "../../components/InfoBox";
+import { router } from "expo-router";
 
 const Profile = () => {
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
+  const { data: posts } = useAppwrite(() => getUserPosts(user.$id));
+
+  const logOut = async () => {
+    await signOut()
+    setUser(null)
+    setIsLoggedIn(false)
+
+    router.replace('/sign-in')
+  };
+
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView className="bg-primary-black h-full">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <VideoCard video={item} />}
+        ListHeaderComponent={() => (
+          <View className="w-full justify-center items-center mt-6 mb-12 px-4">
+            <TouchableOpacity
+              className="w-full items-end mb-10"
+              onPress={logOut}
+            >
+              <Image
+                source={icons.logout}
+                resizeMode="contain"
+                className="w-6 h-6"
+              />
+            </TouchableOpacity>
+            <View className="w-16 h-16 border-secondary-yellow rounded-lg justify-center items-center">
+              <Image
+                source={{ uri: user?.avatar }}
+                className="w-[90%] h-[90%] rounded-lg"
+                resizeMode="cover"
+              />
+            </View>
+            <InfoBox
+              title={user?.username}
+              containerStyles="mt-5"
+              titleStyles="text-lg"
+            />
+            <View className="mt-5 flex-row">
+              <InfoBox
+                title={posts.length || 0}
+                subtitle="Posts"
+                containerStyles="mt-5"
+                titleStyles="text-lg"
+              />
+              <InfoBox
+                title="1.5k"
+                subtitle="Followers"
+                containerStyles="mt-5"
+                titleStyles="text-xl"
+              />
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No images Found"
+            subtitle="No video found for this search"
+          />
+        )}
+      />
+    </SafeAreaView>
   );
 };
 
